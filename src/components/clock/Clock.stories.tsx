1214e74cd20967b1c898910c769138ccb0e52de3
고동현 <dhjk35@naver.com>
2023-01-06 02:51:00 +0900

    feat(Clock): 시계 컴포넌트 구현

---
+++ src/components/clock/Clock.stories.tsx
@@ -0,0 +1,26 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Clock from './Clock';
+
+export default {
+  title: 'components/clock',
+  component: Clock,
+  decorators: [
+    (Story) => (
+      <div style={{ backgroundColor: '#444' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Clock>;
+
+const Template: ComponentStory<typeof Clock> = ({ ...args }) => {
+  return <Clock {...args} />;
+};
+
+export const Default = Template.bind({});
+
+export const IncludeSecond = Template.bind({});
+IncludeSecond.args = {
+  includeSecond: true,
+};
