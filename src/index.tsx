cf9ee68341d58e55d9dce63242ea653eaeef05d3
고동현 <dhjk35@naver.com>
2023-01-05 20:19:20 +0900

    feat: redux 설치 및 기본 구조 구현
    - types 폴더에 user 인터페이스 정의

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
+import store from '@/redux/store';
 import GlobalStyle from '@/styles/globalStyle';
 import { theme } from '@/styles/theme';
 
@@ -15,7 +17,9 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>,
 );
