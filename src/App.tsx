4a0e8887301f1df004ebc76bd21cff1550f86edb
고동현 <dhjk35@naver.com>
2023-01-06 11:09:16 +0900

    feat(App): 리덕스를 사용해 초기 데이터 가져오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import { Provider } from 'react-redux';
+import type { RootState } from '@/store';
+
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import store from '@/redux/store';
 import GlobalStyle from '@/styles/globalStyle';
 import { theme } from '@/styles/theme';
+import { getUserList } from '@/store/usersActions';
+import { useAppDispatch, useAppSelector } from '@/hooks';
 
 import router from './router';
 
 const App = () => {
+  const dispatch = useAppDispatch();
+  const { isLoading, error } = useAppSelector((state: RootState) => state);
+
+  useEffect(() => {
+    dispatch(getUserList());
+  }, [dispatch]);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
+      {error && <div>{error.message}</div>}
+      {isLoading ? <div>Loading...</div> : <RouterProvider router={router} />}
     </ThemeProvider>
   );
 };
