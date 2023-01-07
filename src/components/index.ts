944f0405752c8068e9e2edf28901696e8d854b8b
고동현 <dhjk35@naver.com>
2023-01-07 15:53:42 +0900

    chore: 불필요한 파일들 제거

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,3 @@ export { Header } from './header';
 export { Layout } from './layout';
 export { Selectbox } from './selectbox';
 export { List } from './list';
-export { UserList } from './userList';
