ecea6647deb2f0fb4b78408f41cda43d52dea669
고동현 <dhjk35@naver.com>
2023-01-07 22:52:26 +0900

    refactor(redux): 체크된 유저 목록, 유저 정렬 기능 추가
    - 두 개의 리스트가 화면에 보일 때, 두 목록을 각가 정렬할 수 있게 하기
      위해 체크된 유저 목록을 따로 상태로 만들었습니다.
    - 정렬 기능이 각 리스트에서 호출되지 않고, 한번에 처리할 수 있도록 리듀서에
      정의했습니다.

--- src/store/usersSlice.ts
+++ src/store/usersSlice.ts
@@ -1,13 +1,21 @@
-import type { User } from '@/types';
+import type { Order, User } from '@/types';
 import type { PayloadAction, SerializedError } from '@reduxjs/toolkit';
 
 import { createSlice } from '@reduxjs/toolkit';
 
+import { orderByDate } from '@/utils/users';
+
 import { getUserList } from './usersActions';
+
+interface OrderOption {
+  order: Order;
+  isCheckedUsers: boolean;
+}
 
 interface UsersState {
   initialUsers: User[];
   users: User[];
+  checkedUsers: User[];
   selectedUser: User | undefined;
   isLoading: boolean;
   error: SerializedError | null;
@@ -16,6 +24,7 @@ interface UsersState {
 const initialState: UsersState = {
   initialUsers: [],
   users: [],
+  checkedUsers: [],
   selectedUser: undefined,
   isLoading: false,
   error: null,
@@ -31,13 +40,37 @@ const usersSlice = createSlice({
     checkUser(state, action: PayloadAction<Pick<User, 'id'>>) {
       const index = state.users.findIndex(({ id }) => id === action.payload.id);
 
-      if (index !== -1) {
-        const current = state.users[index].checked;
-        state.users[index].checked = !current;
+      if (index === -1) {
+        return;
+      }
+
+      const user = state.users[index];
+      user.checked = !user.checked;
+
+      if (user.checked) {
+        state.checkedUsers = [
+          user,
+          ...state.checkedUsers.filter((u) => u.id !== user.id),
+        ];
+      } else {
+        state.checkedUsers = state.checkedUsers.filter((u) => u.id !== user.id);
       }
     },
-    selectUser(state, action: PayloadAction<User>) {
-      state.selectedUser = action.payload;
+    selectUser(state, action: PayloadAction<Pick<User, 'id'>>) {
+      const index = state.users.findIndex(({ id }) => id === action.payload.id);
+
+      if (index !== -1) {
+        state.selectedUser = state.users[index];
+      }
+    },
+    orderUsers(state, action: PayloadAction<OrderOption>) {
+      if (action.payload.isCheckedUsers) {
+        const checkedUsers = [...state.checkedUsers];
+        state.checkedUsers = orderByDate(checkedUsers, action.payload.order);
+      } else {
+        const users = [...state.users];
+        state.users = orderByDate(users, action.payload.order);
+      }
     },
     resetUsers(state) {
       state.users = state.initialUsers;
@@ -50,6 +83,7 @@ const usersSlice = createSlice({
     builder.addCase(getUserList.fulfilled, (state, action) => {
       state.initialUsers = action.payload;
       state.users = action.payload;
+      state.checkedUsers = action.payload.filter((user) => user.checked);
       state.isLoading = false;
     });
     builder.addCase(getUserList.rejected, (state, action) => {
@@ -61,6 +95,7 @@ const usersSlice = createSlice({
 
 const { actions, reducer } = usersSlice;
 
-export const { checkUser, selectUser, setUsers, resetUsers } = actions;
+export const { checkUser, selectUser, setUsers, orderUsers, resetUsers } =
+  actions;
 
 export default reducer;
