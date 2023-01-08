8bcc8d45ec7b8f704952be521b54114c9f981bef
고동현 <dhjk35@naver.com>
2023-01-09 04:09:54 +0900

    feat(User, UserDetail): 유저 페이지 및 유저 상세 페이지 구현

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,5 +1,65 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+import { List, Profile } from '@/components';
+import { useAppDispatch, useAppSelector } from '@/hooks';
+import { selectUser } from '@/store/usersSlice';
+
 const User = () => {
-  return <div>유저</div>;
+  const dispatch = useAppDispatch();
+  const { checkedUsers } = useAppSelector((state) => state);
+
+  const handleClickUser = useCallback(
+    (e: React.MouseEvent) => {
+      const clickedUserId = Number((e.currentTarget as HTMLElement).id);
+
+      dispatch(selectUser({ id: clickedUserId }));
+    },
+    [dispatch],
+  );
+
+  return (
+    <Container>
+      <List
+        users={checkedUsers}
+        withProfile
+        isCheckedUsers
+        onUserClick={handleClickUser}
+      />
+      <Profile />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: flex;
+  gap: 20px;
+  max-width: 622px;
+  height: 425px;
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    /* flex-direction: column;
+    justify-content: space-between;
+    gap: 0; */
+    position: relative;
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding: 20px 20px 30px 20px;
+
+    & > div:nth-of-type(1) {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      height: 284px;
+      padding: 0 20px 30px 20px;
+      z-index: 10;
+    }
+
+    & > div:nth-of-type(2) {
+    }
+  }
+`;
 
 export default User;
--- src/pages/user/UserDetail.tsx
+++ src/pages/user/UserDetail.tsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
+import { Profile } from '@/components';
+
 const UserDetail = () => {
-  return <div>유저 상세 페이지</div>;
+  return (
+    <Container>
+      <Profile fullWidth />;
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 622px;
+
+  @media ${({ theme }) => theme.breakPoints.mobile} {
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+  }
+`;
 
 export default UserDetail;
