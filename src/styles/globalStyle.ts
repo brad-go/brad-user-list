66cb31467f45189baf931c48ecdeac72500ff3dc
고동현 <dhjk35@naver.com>
2023-01-09 04:06:19 +0900

    style: 폰트 두께가 적용되지 않던 오류 수정

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -9,6 +9,29 @@ const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: 'SUIT Variable';
     src: url(${SUITVariable}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'SUIT Variable';
+    src: url(${SUITVariable}) format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'SUIT Variable';
+    src: url(${SUITVariable}) format('truetype');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'SUIT Variable';
+    src: url(${SUITVariable}) format('truetype');
+    font-weight: 700;
+    font-style: normal;
   }
 
   *, *::before, *::after {
